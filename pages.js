fdd9de6ec3235b48831e7ddabcf4f2d59cb43192
web3iscool <110189359+web3iscool@users.noreply.github.com>
2024-01-09 16:32:08 +0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/web3iscool/web3iscool.github.io.git";
+const repoURL = "https://github.com/web3iscool/anyabindra.git";
 
 ghpages.publish(
   pathname,
